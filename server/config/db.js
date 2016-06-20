3f1dcd6da3104afe4499e711ac64152d775dbf01
Soni Pandey <sonipandey.71@gmail.com>
2016-06-20 17:20:01 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,11 @@
-var Mongoose = require('mongoose');
-var config = require('./config');
+const Mongoose = require('mongoose')
+const config = require('./config')
 
 Mongoose.connect(config.database.url);  
-var db = Mongoose.connection;
+const db = Mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function callback() {
     console.log("Connection with database succeeded.");
 });
 
-exports.Mongoose = Mongoose;
 exports.db = db;
